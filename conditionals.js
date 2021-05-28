17bba1804d8ebe15afdf20a7c19d50ceef525efa
AlSid604 <alex_sidor97@hotmail.com>
2021-05-28 19:10:54 +0000

    conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,14 @@
-if !(raining) {
-  console.log("Leave your umbrella at home!");
+const age = 13
+
+function whichSchool(age) {
+  if (age < 13) { 
+    return ("Elementary School")
+  }
+
+   else if (age >= 13 && age <= 18) { 
+    return ("Secondary School")
+  } else {
+    
+    return ("LightHouse Labs")
+  }
 }
